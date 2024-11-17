f95181ec43a44335bae8904205058f92bd6b3d53
qiyu666 <3270581950@qq.com>
2024-11-17 10:55:27 +0800

    Add files via upload

---
+++ js/footer.js
@@ -0,0 +1,24 @@
+console.log(navigator.userAgent)
+console.log(window.location.hostname)
+if( /LieBao|HMSCore|HarmonyOS|HUAWEI|Pixel|baiduboxapp|MicroMessenger|UCWEB|UCBrowser|XiaoMi|Miui|SAMSUNG|phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone/i.test(navigator.userAgent) &&
+	(window.location.hostname == 'www.pcgeshi.com' || window.location.hostname == 'pcgeshi.com')){
+	location.href = "https://m.pcgeshi.com";
+}
+document.write('<div style="display:none">');
+document.writeln("<script>");
+document.writeln("var _hmt = _hmt || [];");
+document.writeln("(function() {");
+document.writeln("  var hm = document.createElement(\"script\");");
+document.writeln("  hm.src = \"https://hm.baidu.com/hm.js?f88d7433b78867bcfd95be3b8149cfb7\";");
+document.writeln("  var s = document.getElementsByTagName(\"script\")[0]; ");
+document.writeln("  s.parentNode.insertBefore(hm, s);");
+document.writeln("})();");
+document.writeln("</script>");
+document.write('</div>');
+document.write('<div class="footer"><div class="w1040"><div class="geshi"><h1>格式工厂</h1>是一款免费多功能的多媒体格式转换软件。<br />格式工厂，您得力的转换帮手！<br />本软件使用了FFmpeg解码库<br />&copy; 2013 Pcfreetime.com, All rights reserved. 版权所有</div><div class="link"><ul><li><a href="download.html">软件下载</a></li><li><a href="screen.html">软件截图</a></li><li><a href="log.html">更新日志</a></li><li><a href="faq.html">常见问题</a></li><li><a href="about.html">关于</a></li></ul></div><div class="yq_link"><h1>友情链接</h1><ul>');
+document.write('<li><a href="http://soft.qq.com" target="_blank">腾讯软件中心</a></li>');
+document.write('<li><a href="https://www.baidu.com/s?wd=%E6%A0%BC%E5%BC%8F%E5%B7%A5%E5%8E%82" target="_blank">视频格式转换</a></li>');
+document.write('<li><a href="http://www.yyzsoft.com" target="_blank">视频剪辑软件</a></li>');
+document.write('<li><a href="https://www.shudaxia.com/" target="_blank">鼠标连点器</a></li>');
+document.write('</ul></div><div class="sw_link"><h1 class="sw">商务合作</h1><ul><li><em class="email"></em>Email：pcfreetime@126.com</li><li></li></ul></div></div></div><div style="display:none"><script language="javascript" type="text/javascript" src="http://js.users.51.la/16647364.js"></script></div>');
+document.write('<div style="text-align: center;margin:0 auto; padding:20px 0;"><a href="https://beian.miit.gov.cn/" target="_blank">沪ICP备15025014号-2</a>&nbsp;&nbsp;&nbsp;<a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=31011502009137" style="display:inline-block;text-decoration:none;height:20px;line-height:20px;"><img src="./img/gonganbeian.png" style="float:left;"/>&nbsp;沪公网安备 31011502009137号</a></div>');
